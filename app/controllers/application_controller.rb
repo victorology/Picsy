960fe1d717ad508c7ba14727754ec0ce645b13bf
Aditya Agustyana <aditya@prawirasoft.com>
2011-07-12 16:05:56 +0700

    m2day connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < ActionController::Base
           :error_message => "you need to logged in to access this feature",
           :value => nil
         }
+        session[:me2day] = {:id => params[:id], :session_api => params[:session_api]} if params[:controller] == "me2day"
         render :json => JSON.generate(@raw_result)
         return false
       end 
--- app/controllers/me2day_controller.rb
+++ app/controllers/me2day_controller.rb
@@ -31,7 +31,8 @@ class Me2dayController < ApplicationController
       )
       
       if @client.noop["message"] == "성공했습니다."
-        current_user.update_attributes(
+        @api_user = User.find(:first, :conditions => {:id => session[:me2day][:id], :session_api => session[:me2day][:session_api]}) 
+        @api_user.current_user.update_attributes(
           {
             :me2day_key => params[:user_key],
             :me2day_id => params[:user_id]  
