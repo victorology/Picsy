084b2d7728de7ea33273c9e720a7b4d53f3c234a
Aditya Agustyana <aditya.jamop@gmail.com>
2011-11-29 11:55:01 +0700

    checking expired me2day key

--- app/controllers/utilities_controller.rb
+++ app/controllers/utilities_controller.rb
@@ -6,6 +6,7 @@ class UtilitiesController < ApplicationController
     result[:facebook] = check_facebook_key
     result[:tumblr] = check_tumblr_key
     result[:twitter] = check_twitter_key
+    result[:me2day] = check_me2day_key
     
     respond_to do |format|
       format.json {
@@ -105,7 +106,29 @@ class UtilitiesController < ApplicationController
   end
   
   def check_me2day_key
-    
+    rs = nil
+    if current_user.me2day_connected? == true
+      begin
+        @client = Me2day::Client.new(
+          :user_id => current_user.me2day_id, 
+          :user_key => current_user.me2day_key, 
+          :app_key => ME2DAY_KEY
+        )
+        if JSON.parse(@client.noop)["code"].to_s == "0"
+          info = JSON.parse(@client.get_person(current_user.me2day_id))
+          rs = {
+            :expired => false,
+            :info => info.select{|k,v| ["id", "nickname", "description", "email", "friendsCount"].include?(k)}
+          }
+        else
+          me2day_expired
+        end
+      rescue Me2day::Client::UnauthenticatedError
+        me2day_expired
+      end
+    else
+      me2day_expired
+    end
   end
   
   def twitter_expired
@@ -147,7 +170,18 @@ class UtilitiesController < ApplicationController
       :info => nil
     }
   end  
-  
-         
 
+  def me2day_expired
+    current_user.update_attributes(
+      :me2day_key => nil,
+      :me2day_id => nil,
+      :me2day_nickname => nil
+    )
+    
+    rs = {
+      :expired => true,
+      :info => nil
+    }
+  end  
+  
 end
